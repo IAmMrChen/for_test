32c2057112e24dc7c866ccc321974225501b5a91
陈艺超 <791293535@qq.com>
2026-05-31 13:25:44 +0800

    修改配置，增加 git 忽略文件。

--- parent-child-miniprogram/config/env.js
+++ parent-child-miniprogram/config/env.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://127.0.0.1:8080'
+// export const API_BASE_URL = 'http://127.0.0.1:8080'
+export const API_BASE_URL = 'https://test-edu.coolcyc.com'
 
 export const DEMO_USER = {
   userId: 1001,
