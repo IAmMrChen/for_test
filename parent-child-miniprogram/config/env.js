41b352101bf94e1ff6da47ec34234e0080a9e2d4
陈艺超 <791293535@qq.com>
2026-05-31 13:34:39 +0800

    增加忽略文件

--- parent-child-miniprogram/config/env.js
+++
@@ -1,7 +0,0 @@
-// export const API_BASE_URL = 'http://127.0.0.1:8080'
-export const API_BASE_URL = 'https://test-edu.coolcyc.com'
-
-export const DEMO_USER = {
-  userId: 1001,
-  nickname: '演示用户'
-}
